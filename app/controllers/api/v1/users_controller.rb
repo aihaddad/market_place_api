70a20eb50447ad7e2c3936e31a9f31189968fe46
Ahmed Elhaddad <ahmed@example.com>
2014-09-23 20:23:53 +0300

    Adds user show and create features

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,14 +5,14 @@ class Api::V1::UsersController < ApplicationController
     respond_with User.find(params[:id]) 
   end
 
-  # def create
-  #   user = User.new(user_params) 
-  #   if user.save
-  #     render json: user, status: 201, location: [:api, user] 
-  #   else
-  #     render json: { errors: user.errors }, status: 422
-  #   end
-  # end
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      render json: @user, status: 201, location: [:api, @user] 
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
 
   # def update
   #   user = User.find(params[:id]) 
@@ -30,9 +30,9 @@ class Api::V1::UsersController < ApplicationController
   #   head 204
   # end
 
-  # private
+  private
 
-  #   def user_params
-  #     params.require(:user).permit(:email, :password, :password_confirmation) 
-  #   end
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation) 
+    end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Api::V1::UsersController do
 
   describe "GET #show" do
-    before(:each) do
+    before :each do
       @user = FactoryGirl.create :user
       get :show, id: @user.id, format: :json
     end
@@ -12,8 +12,46 @@ describe Api::V1::UsersController do
 
     it "returns the right user information" do
       user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email]).to eql @user.email 
+      expect(user_response[:email]).to eql @user.email
     end
-  end 
+  end #show
+
+  describe "POST #create" do
+
+    context "when it is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when email is not given" do
+      before(:each) do
+        #notice I'm not including the email
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json clarifying that email cannot be blank" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end #create
 
 end
