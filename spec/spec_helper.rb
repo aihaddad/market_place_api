395de45607bfdd801819d8cb4025e17669bacd9a
Ahmed Elhaddad <ahmed@example.com>
2014-09-28 10:43:18 +0300

    Adds sessions#create and signing in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,8 +41,9 @@ RSpec.configure do |config|
   config.order = "random"
 
   #Including to test requests
-  config.include Request::JsonHelpers, :type => :controller
-  config.include Request::HeadersHelpers, :type => :controller
+  config.include Request::JsonHelpers,    type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+  config.include Devise::TestHelpers,     type: :controller
 
   config.before(:each, type: :controller) do
     include_default_accept_headers
