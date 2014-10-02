9a95b746dff00a408de3615e6c9b229ab1a63f94
Ahmed Elhaddad <ahmed@example.com>
2014-10-02 19:48:46 +0300

    Adds organization/filtering scopes to product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,4 +17,48 @@ describe Product do
   it { should validate_presence_of :user_id }
 
   it { should belong_to :user }
+  
+  describe "organized" do
+      before(:each) do
+        @product1 = FactoryGirl.create :product, title: "A plasma TV",    price: 100
+        @product2 = FactoryGirl.create :product, title: "Fastest Laptop", price: 50
+        @product3 = FactoryGirl.create :product, title: "CD player",      price: 150
+        @product4 = FactoryGirl.create :product, title: "LCD TV",         price: 99
+      end
+    
+    describe "with title filter set to 'TV'" do
+      it "returns only 2 products" do
+        expect(Product.filter_by_title("TV").size).to eql 2
+      end
+
+      it "returns the matching products" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end # title filter
+
+    describe "with price filter" do
+      context "above certain price" do
+        it "returns the products which are above or equal to filter" do
+          expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+        end
+      end
+
+      context "below certain price" do
+        it "returns the products which are below or equal to filter" do
+          expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
+        end
+      end
+    end # price filter
+    
+    describe "with decending sort by most recent" do
+      before do
+        @product2.touch
+        @product3.touch
+      end
+      
+      it "returns the most recently updated first" do
+        expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+      end
+    end # sort most recent
+  end
 end
